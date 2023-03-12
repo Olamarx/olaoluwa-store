2a156165bbe642fb964e235c4a3746511ee8ad6b
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-12 10:00:01 +0100

    Import components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import { Container } from 'react-bootstrap';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <main>
-      Olaoluwa Shopping Store
-    </main>
-  );
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          Olaoluwa Shopping Store
+        </Container>
+      </main>
+      <Footer />
+    </>
+    );
 }
 
 export default App;
