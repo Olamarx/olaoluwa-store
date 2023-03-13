b91079be17fbaab1cb1cdcc23c45ac0015e27b53
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-13 17:54:56 +0100

    Add Routing to the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,42 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Container } from 'react-bootstrap';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <main className="py-3">
+              <Container>
+                <HomeScreen />
+              </Container>
+            </main>
+          </>
+        } />
+        
+        <Route path="/product/:id" element={
+          <>
+            <main className="py-3">
+              <Container>
+                <ProductScreen />
+              </Container>
+            </main>
+          </>
+        } />
+
+      </Routes>
+
       <Footer />
-    </>
-    );
+    </BrowserRouter>
+  );
 }
 
 export default App;
