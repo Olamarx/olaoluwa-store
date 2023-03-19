afbcb6ff60a68f1a3121a7ace41e3b2e65589077
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-19 12:12:03 +0100

    use useState and useEffect

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Col, Row } from 'react-bootstrap'
-import products from '../products'
 import Product from '../components/Product'
 
 const HomeScreen = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      const result = await data
+      setProducts(result)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <>
     <h1>Latest Products</h1>
