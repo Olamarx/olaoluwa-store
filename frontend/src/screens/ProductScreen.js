6f4aafb9246ab2d9718c14841125e3f78071067e
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-19 12:12:13 +0100

    use useState and useEffect

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
 
 
 const ProductScreen = () => {
-  const { id } = useParams()
-  const product = products.find(p => p._id === id)
+  const [product, setProduct] = useState({})
 
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+      const result = await data
+      setProduct(result)
+    }
+
+    fetchProduct()
+  }, [id])
 
   return (
     <>
