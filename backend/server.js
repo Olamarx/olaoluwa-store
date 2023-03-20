e7ec3348fb0cad7d0cf9eb98ea4ebfef82234730
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-20 20:28:41 +0100

    Add ENV file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.mjs'
+import products from './data/products.js'
 
 dotenv.config()
 
@@ -19,4 +19,6 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
