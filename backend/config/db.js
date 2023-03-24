90a391fe929797a06596bdcbf41394564c9e5dd7
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-24 15:15:48 +0100

    used color

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@ const connectDB = async () => {
  try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
     
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error: ${error.message}`.red.bold.underline);
     process.exit(1)
   }
 }
