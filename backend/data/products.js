1bb149aea048e2f7bd6a9ccd8fb822e85386a5ba
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-30 10:51:31 +0100

    removed id

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
 const products = [
   {
-    _id: '1',
     name: 'Airpods Wireless Bluetooth Headphones',
     image: '/images/airpod.png',
     description: 'Bluetooth technology helps you in connecting it with compatible devices wirelessly High-wuality AAC audio offers immersive listening experience built-in microphone allows you to take calls while working',
@@ -12,7 +11,6 @@ const products = [
     numReviews: 4,
   },
   {
-    _id: '2',
     name: 'Iphone 11 Pro Max',
     image: '/images/iphone11.jpg',
     description: 'Introducing Iphone 11 Pro Max. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
@@ -24,7 +22,6 @@ const products = [
     numReviews: 4,
   },
   {
-    _id: '3',
     name: 'Cannon EOS 80D DSLR Camera',
     image: '/images/camera.png',
     description: 'Characterised by vrtsatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
@@ -36,7 +33,6 @@ const products = [
     numReviews: 3,
   },
   {
-    _id: '4',
     name: 'Sony Playstation 4 White Version',
     image: '/images/white-playstation-4.jpg',
     description: 'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
