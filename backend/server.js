b31060be4c1f5b1c662a78590f7deda95fe3b10c
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-31 14:35:08 +0100

    Add server error handler

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,23 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config()
-
 connectDB()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('API is running')
+app.use((req, res, next) => {
+  console.log('HELLO');
+  next()
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
