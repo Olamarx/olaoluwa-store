cfbf8741abd7820fe35269d1eb06a23349968791
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-01 12:52:52 +0100

    Add individual product reducer

--- frontend/src/redux/reducers/productReducer.js
+++ frontend/src/redux/reducers/productReducer.js
@@ -7,9 +7,9 @@ export const productListReducer = (state = {products: []}, action) => {
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload }
     case PRODUCT_LIST_FAIL:
-      return { loading: false, products: [] }
+      return { loading: false, error: action.payload }
   
     default:
-      break;
+      return state;
   }
 }
