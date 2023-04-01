a9ee1a87e12cf3cad46a59496793e43cd2c1b503
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-01 12:52:06 +0100

    Add types

--- frontend/src/redux/types.js
+++ frontend/src/redux/types.js
@@ -1,3 +1,7 @@
 export const PRODUCT_LIST_REQUEST = 'product/list/REQUEST';
 export const PRODUCT_LIST_SUCCESS = 'product/list/SUCCESS';
 export const PRODUCT_LIST_FAIL = 'product/list/FAIL';
+
+export const INDIVIDUAL_PRODUCT_REQUEST = 'individual/product/REQUEST';
+export const INDIVIDUAL_PRODUCT_SUCCESS = 'individual/product/SUCCESS';
+export const INDIVIDUAL_PRODUCT_FAIL = 'individual/product/FAIL';
