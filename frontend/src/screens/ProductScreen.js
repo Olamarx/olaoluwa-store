4b0c84debe37879310333ce2dda1a77352c144b0
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-02 11:27:47 +0100

    Added qty

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Form, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { indProduct } from '../redux/actions/productActions';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 
 const ProductScreen = () => {
+const [qty, setQty] = useState(0)
+
   const dispatch = useDispatch()
   const { id } = useParams()
   const oneProduct = useSelector((state) => state.product)
@@ -71,6 +73,24 @@ const ProductScreen = () => {
           </Row>
         </ListGroup.Item>
         <ListGroup.Item>
+        { product.countInStock > 0 && (
+
+          <ListGroup.Item>
+            <Row>
+              <Col>Qty</Col>
+              <Col>
+                <Form.Control as="select" value={qty} onChange={(e) => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                    {x + 1}
+                    </option>
+                  ))}
+                </Form.Control>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        ) }
+
           <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
           Add to cart
           </Button>
