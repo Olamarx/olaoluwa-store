cee3b4c892f2946a63dcbe4b1a25403c6e5112c1
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-02 17:15:59 +0100

    Add cart types

--- frontend/src/redux/types.js
+++ frontend/src/redux/types.js
@@ -5,3 +5,6 @@ export const PRODUCT_LIST_FAIL = 'product/list/FAIL';
 export const INDIVIDUAL_PRODUCT_REQUEST = 'individual/product/REQUEST';
 export const INDIVIDUAL_PRODUCT_SUCCESS = 'individual/product/SUCCESS';
 export const INDIVIDUAL_PRODUCT_FAIL = 'individual/product/FAIL';
+
+export const CART_ADD_ITEM = 'cart/item/ADD';
+export const CART_REMOVE_ITEM = 'cart/item/REMOVE';
