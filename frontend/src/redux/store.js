b359198afa75d8723d89e879ba77621160c5d078
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-02 17:15:39 +0100

    Add cartReducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,12 +3,19 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer } from './reducers/productReducer';
 import { indProductReducer } from './reducers/indProduct';
+import { cartReducer } from './reducers/cartReducer';
+
 const reducer = combineReducers({
   productList: productListReducer,
   product: indProductReducer,
+  cart: cartReducer,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+  cart: {cartItems: cartItemsFromStorage}
+}
 
 const middleware = [thunk]
 
