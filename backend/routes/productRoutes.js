c9d615a9b610a53a29e0dde2262e0e4f9a7dda9d
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-08 22:10:32 +0100

    update productroutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { getProductById, getProducts } from '../controllers/productController.js';
 const router = express.Router()
 
-
 router.route('/').get(getProducts)
 
 router.route('/:id').get(getProductById)
