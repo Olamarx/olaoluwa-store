4c95dd63a660053d814f87b582038ce717dcac79
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-15 11:55:10 +0100

    Finalized user login/backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import protect from '../middleware/authMiddleware.js';
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfile } from '../controllers/userController.js';
 const router = express.Router()
 
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
