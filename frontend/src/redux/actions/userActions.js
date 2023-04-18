1b5fbf1b496c775fa23894fc202d98a3d57baeae
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-18 18:55:20 +0100

    Add user action creator

---
+++ frontend/src/redux/actions/userActions.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../types"
+
+export const login = (email, password) => async (dispatch) => {
+try {
+  dispatch({
+    type: USER_LOGIN_REQUEST,
+  })
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  const { data } = await axios.post('/api/users/login', { email, password }, config)
+
+  dispatch({
+    type: USER_LOGIN_SUCCESS,
+    payload: data
+  })
+} catch (error) {
+  
+}
+}
