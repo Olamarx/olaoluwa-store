cfad06a0dca3c32855643e63c09444a5ec8e07bf
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-18 18:55:00 +0100

    Add user login types

--- frontend/src/redux/types.js
+++ frontend/src/redux/types.js
@@ -8,3 +8,10 @@ export const INDIVIDUAL_PRODUCT_FAIL = 'individual/product/FAIL';
 
 export const CART_ADD_ITEM = 'cart/item/ADD';
 export const CART_REMOVE_ITEM = 'cart/item/REMOVE';
+
+export const USER_LOGIN_REQUEST = 'user/login/REQUEST';
+export const USER_LOGIN_SUCCESS = 'user/login/SUCCESS';
+export const USER_LOGIN_FAILED = 'user/login/FAILED';
+
+export const USER_LOGOUT = 'user/logout';
+
