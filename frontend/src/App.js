e1d6adb0b23eb1135971d1d0d37f791dfc0c5e41
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-04-21 17:47:45 +0100

    refactored and added login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,45 +6,22 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
-  // const match = useRouteMatch
   return (
     <BrowserRouter>
       <Header />
-
-      <Routes>
-        <Route path="/" element={
-          <>
-            <main className="py-3">
-              <Container>
-                <HomeScreen />
-              </Container>
-            </main>
-          </>
-        } />
-        
-        <Route path="/product/:id" element={          
-          <>
-            <main className="py-3">
-              <Container>
-                <ProductScreen /> 
-              </Container>
-            </main>
-          </>
-        } />
-
-        <Route path="/cart/:id?" element={          
-          <>
-            <main className="py-3">
-              <Container>
-                <CartScreen /> 
-              </Container>
-            </main>
-          </>
-        } />
-      </Routes>
-
+      <main className="py-3">
+        <Container>
+          <Routes>
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
+          <Route path="/cart/:id?" element={<CartScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </Container>
+      </main>
       <Footer />
     </BrowserRouter>
   );
