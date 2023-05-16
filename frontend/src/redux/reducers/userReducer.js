5eb323f8a083f8263ac55645690e082de91f2825
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-05-16 23:57:26 +0100

    Added the updateUserProfile reducer method

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAILED, USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAILED, USER_DETAIL_REQUEST, USER_DETAIL_SUCCESS, USER_DETAIL_FAILED } from "../types";
+import { USER_LOGIN_FAILED, USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAILED, USER_DETAIL_REQUEST, USER_DETAIL_SUCCESS, USER_DETAIL_FAILED, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAILED } from "../types";
 
 export const userLoginReducer = (state = { }, action) => {
   switch (action.type) {
@@ -37,6 +37,20 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     case USER_DETAIL_SUCCESS:
       return { loading: false, user: action.payload }
     case USER_DETAIL_FAILED:
+      return { loading: false, error: action.payload }
+  
+    default:
+      return state;
+  }
+}
+
+export const userUpdateProfileReducer = (state = { }, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true, userInfo: action.payload }
+    case USER_UPDATE_PROFILE_FAILED:
       return { loading: false, error: action.payload }
   
     default:
