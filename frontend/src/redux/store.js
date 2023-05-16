e692f8dc568f6e44a95d879a77aafbc8e8b3d336
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-05-16 23:59:36 +0100

    Added the reducers to the store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer } from './reducers/productReducer';
 import { indProductReducer } from './reducers/indProduct';
 import { cartReducer } from './reducers/cartReducer';
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducer';
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducer';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -13,6 +13,7 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
