7a7f08adad6923c10a7a5c4c22d4dbafd24acd73
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-05-18 22:57:09 +0100

    Added shipping address actions

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../types";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../types";
 
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -21,9 +21,12 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 }
 
 export const removeFromCart = (id) => async (dispatch, getState) => {
-  try {
     dispatch({ type: CART_REMOVE_ITEM, payload: id })
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
-  } catch (error) {
-  }
+
+}
+
+export const saveShippingAddress = (data) => async (dispatch) => {
+    dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data })
+    localStorage.setItem("shippingAddress", JSON.stringify(data))
 }
