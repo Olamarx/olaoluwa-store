78e84fa85f4344b1808af848444ccac5e2e6b38a
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-05-28 21:18:09 +0100

    Added the cart action for payment method

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../types";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS, CART_SAVE_PAYMENT_METHOD } from "../types";
 
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -29,4 +29,9 @@ export const removeFromCart = (id) => async (dispatch, getState) => {
 export const saveShippingAddress = (data) => async (dispatch) => {
     dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data })
     localStorage.setItem("shippingAddress", JSON.stringify(data))
+}
+
+export const savePaymentMethod = (data) => async (dispatch) => {
+    dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data })
+    localStorage.setItem("paymentMethod", JSON.stringify(data))
 }
