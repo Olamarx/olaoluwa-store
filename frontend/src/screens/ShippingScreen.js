6c9529aba8788dbdaa1dbf5e59079ca6027a971c
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-05-28 19:40:14 +0100

    Finalized thr shippingScreen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,20 +3,25 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import { saveShippingAddress } from '../redux/actions/cartActions'
 
 const ShippingScreen = () => {
-
   const cart = useSelector((state) => state.cart)
   const { shippingAddress } = cart
 
-  const [address, setAddress] = useState('')
-  const [city, setCity] = useState('')
-  const [postalCode, setPostalCode] = useState('')
-  const [country, setCountry] = useState('')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  
+
+  const [address, setAddress] = useState(shippingAddress.address)
+  const [city, setCity] = useState(shippingAddress.city)
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+  const [country, setCountry] = useState(shippingAddress.country)
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(e)
+    dispatch(saveShippingAddress({address, city, postalCode, country}))
+    navigate('/payment')
   }
 
 
